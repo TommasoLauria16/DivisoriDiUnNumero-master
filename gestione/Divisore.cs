ee898a70f3b86dd89f9a700f08ba7b6a523e171d
tommaso lauria <tommaso.lauria9@gmail.com>
2020-11-30 14:32:44 +0100

    Add project files.

---
+++ gestione/Divisore.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace Gestione
+{
+    public class Divisore
+    {
+        public static int LeggiNumero()
+        {
+            Console.Write("inserisci un numero intero: ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            return n;
+        }
+        public static void StampaDivisori(int n)
+        {
+            
+            int i = 1,count=1;
+            int max = n / 2;
+            while (i <= max)
+            {
+                if (n % i == 0)
+                {
+                    Console.WriteLine(i);
+                    count++;
+                }
+
+                i++;
+            }
+            Console.WriteLine(n);
+            if (count== 2)
+                Console.WriteLine("il numero è primo");
+            else
+                Console.WriteLine("il numero non è primo");
+
+                
+        }
+    }
+}
---
+++ provacalcolocomputazionale/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Gestione;
+
+namespace provacalcolocomputazionale
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = Divisore.LeggiNumero();
+            Divisore.StampaDivisori(n);
+
+        }
+    }
+}
